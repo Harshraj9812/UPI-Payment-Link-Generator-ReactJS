fe58ea5aa017ea7a30c1e77ae4e81e6ad4dd8ce2
Harsh Raj <harshraj9812@gmail.com>
2022-03-08 14:40:34 +0530

    Added Support Developer QR Code as Default QR and Fixed Pay Now Button is Not Working.

--- src/Components/Pay.js
+++ src/Components/Pay.js
@@ -4,12 +4,19 @@ import { Link } from 'react-router-dom';
 
 
 const Pay = () => {
+  // Getting Get Parameters from GET Request From URL
   const queryParams = new URLSearchParams(window.location.search);
   const pname = queryParams.get('pn');
   const upi = queryParams.get('pa');
   const amount = queryParams.get('am');
 
-  let link = "upi://pay?pn=" + pname + "&pa=" + upi + "&cu=INR&am=" + amount
+  let link = "upi://pay?pn="+pname+"&pa="+upi+"&cu=INR&am="+amount
+  if (pname===null && upi===null && amount===null)
+  {
+    link = "upi://pay?pn=Harsh Raj&pa=7684028503@paytm&cu=INR&am=150"
+  }
+
+  const QrMessage = "Scan To Pay ☝️"
   return (
     <>
       {/* http://localhost:3000/?pn=test&pa=uid&am=100 */}
@@ -18,21 +25,21 @@ const Pay = () => {
         <div className="row mt-5">
 
           <div className="col-md-6 d-flex justify-content-center mt-4 mb-4">
-            <QRCode value={`upi://pay?pn=${pname}&pa=${upi}&cu=INR&am=${amount}`} bgColor='black' fgColor='white' />
+            <QRCode value={`upi://pay?pn=${pname===null?'Harsh Raj':pname}&pa=${upi===null?'7684028503@paytm':upi}&cu=INR&am=${amount===null?'150':amount}`} bgColor='black' fgColor='white' />
           </div>
 
           <form className='col-md-6'>
             <div className="mb-3">
               <label htmlFor="exampleInputEmail1" className="form-label">Paying To -</label>
-              <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={pname} disabled />
+              <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={pname===null?'Harsh Raj':pname} disabled />
             </div>
             <div className="mb-3">
               <label htmlFor="exampleInputPassword1" className="form-label">UPI ID -</label>
-              <input type="email" className="form-control" id="exampleInputPassword1" value={upi} disabled />
+              <input type="email" className="form-control" id="exampleInputPassword1" value={upi===null?'7684028503@paytm':upi} disabled />
             </div>
             <div className="mb-3">
               <label htmlFor="exampleInputPassword1" className="form-label">Amount Paying -</label>
-              <input type="number" className="form-control" id="exampleInputPassword1" value={amount} disabled />
+              <input type="number" className="form-control" id="exampleInputPassword1" value={amount===null?'150':amount} disabled />
             </div>
 
             <a href={link}>
@@ -45,7 +52,7 @@ const Pay = () => {
             </Link>
           </form>
           <div className="col-md-6 d-flex justify-content-center mb-5">
-            <h3 className='text-warning d-none d-sm-none d-md-none d-lg-block d-xl-block'>Scan To Pay</h3>
+            <h3 className='text-warning d-none d-sm-none d-md-none d-lg-block d-xl-block'>{pname===null && upi===null && amount===null?"Support Developer ☝️":QrMessage}</h3>
           </div>
         </div>
       </div>
